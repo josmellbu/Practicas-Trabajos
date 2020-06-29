2b060136b2ea605c97a618560e1d7a3001b549cd
josmellbu <josmell.quispe@upeu.edu.pe>
2020-06-29 11:59:02 -0500

    moviendo

---
+++ Unidad 2/appPagina/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TicksDataModel } from '@syncfusion/ej2-angular-inputs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'appPagina';
+}
+
